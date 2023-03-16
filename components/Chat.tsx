036a2221c3fe25c17a9e7425869e7629808eeb10
Norio Shimizu <toykyo.net@gmail.com>
2023-03-16 19:07:34 +0900

    Update Chat.tsx
    revert

--- components/Chat.tsx
+++ components/Chat.tsx
@@ -13,71 +13,36 @@ export const initialMessages: ChatGPTMessage[] = [
   },
 ]
 
-const InputMessage = ({ input, setInput, sendMessage }: any) => {
-  const [isListening, setIsListening] = useState(false)
-
-  const recognition = (window as any).SpeechRecognition || (window as any).webkitSpeechRecognition
-  recognition.continuous = false
-  recognition.interimResults = false
-  recognition.lang = 'ja-JP'
-
-  recognition.onresult = (event) => {
-    const transcript = event.results[0][0].transcript
-    setInput(transcript)
-    setIsListening(false)
-    recognition.stop()
-  }
-
-  recognition.onerror = () => {
-    setIsListening(false)
-    recognition.stop()
-  }
-
-  return (
-    <div className="mt-6 flex clear-both">
-      <input
-        type="text"
-        aria-label="chat input"
-        required
-        className="min-w-0 flex-auto appearance-none rounded-md border border-zinc-900/10 bg-white px-3 py-[calc(theme(spacing.2)-1px)] shadow-md shadow-zinc-800/5 placeholder:text-zinc-400 focus:border-teal-500 focus:outline-none focus:ring-4 focus:ring-teal-500/10 sm:text-sm"
-        value={input}
-        onKeyDown={(e) => {
-          if (e.key === 'Enter') {
-            sendMessage(input)
-            setInput('')
-          }
-        }}
-        onChange={(e) => {
-          setInput(e.target.value)
-        }}
-      />
-      <Button
-        type="submit"
-        className="ml-4 flex-none"
-        onClick={() => {
+const InputMessage = ({ input, setInput, sendMessage }: any) => (
+  <div className="mt-6 flex clear-both">
+    <input
+      type="text"
+      aria-label="chat input"
+      required
+      className="min-w-0 flex-auto appearance-none rounded-md border border-zinc-900/10 bg-white px-3 py-[calc(theme(spacing.2)-1px)] shadow-md shadow-zinc-800/5 placeholder:text-zinc-400 focus:border-teal-500 focus:outline-none focus:ring-4 focus:ring-teal-500/10 sm:text-sm"
+      value={input}
+      onKeyDown={(e) => {
+        if (e.key === 'Enter') {
           sendMessage(input)
           setInput('')
-        }}
-      >
-        Say
-      </Button>
-      <Button
-        type="button"
-        className={`ml-4 flex-none ${isListening ? 'bg-blue-500' : ''}`}
-        onClick={() => {
-          if (!isListening) {
-            recognition.start()
-          } else {
-            recognition.stop()
-          }
-          setIsListening(!isListening)
-        }}
-      >
-        {isListening ? 'Stop' : 'Voice'}
-      </Button>
-    </div>
-  )
-}
+        }
+      }}
+      onChange={(e) => {
+        setInput(e.target.value)
+      }}
+    />
+    <Button
+      type="submit"
+      className="ml-4 flex-none"
+      onClick={() => {
+        sendMessage(input)
+        setInput('')
+      }}
+    >
+      Say
+    </Button>
+  </div>
+)
 
 export function Chat() {
   const [messages, setMessages] = useState<ChatGPTMessage[]>(initialMessages)
